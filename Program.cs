1ced75acac0927da59cca34d31b856b73438639f
Sandorivera <gabrielriveraarn05@gmail.com>
2025-01-07 16:13:38 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Leccion 22-Excepciones\Exampl.txt");
+                Console.WriteLine(content);
+                Console.ReadLine();
+            }
+            catch (FileNotFoundException ex) 
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly: Example.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(@"Make sure the name of the diectory C:\Leccion 22-Excepciones exists.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            { 
+                // Code to finalize
+                // Setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now..."); 
+            }
+        }
+    }
+}
